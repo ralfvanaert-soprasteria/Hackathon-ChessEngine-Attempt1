f7a289b1cac282083b1a6532cea1d1906685ef34
rpjvanaert <ralf@van-aert.nl>
2026-01-29 19:59:59 +0100

    feat: initial setup

---
+++ src/main/java/sopra/steria/App.java
@@ -0,0 +1,13 @@
+package sopra.steria;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println( "Hello World!" );
+    }
+}
